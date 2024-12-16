28620d4a953a51c516a0f5114e78b7f558627c4a
minseobshim <mshim@blast-team.com>
2024-12-16 17:52:56 +0900

    refactor: 코드 간결화

--- fizzbuzz_심민섭.py
+++ fizzbuzz_심민섭.py
@@ -1,11 +1,6 @@
 for i in range(21):
-    if i % 3 == 0:
-        print('fizz')
-    elif i % 5 == 0:
-        print('buzz')
-    elif i % 15 == 0:
-        # 15의 배수일 경우
-        print('fizzbuzz')
+    if i % 3 == 0 or i % 5 == 0:
+        print('fizz' * (i % 3 == 0) + 'buzz' * (i % 5 ==0))
     else:
         print(i)
 
